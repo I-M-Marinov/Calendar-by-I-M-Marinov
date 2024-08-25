5237ad145e4fea01ad628c746d8a732bf6f64fe3
I-M-Marinov <lcfrrr@gmail.com>
2024-08-25 21:51:41 +0300

    Make the CreateCalendar method in the service class asynchonous
    Make the CreateCalendar method in the service class asynchonous

--- Calendar-by-I-M-Marinov/Services/GoogleCalendarService.cs
+++ Calendar-by-I-M-Marinov/Services/GoogleCalendarService.cs
@@ -261,16 +261,15 @@ public class GoogleCalendarService : IGoogleCalendarService
 		await request.ExecuteAsync();
 	}
 
-	public Calendar CreateCalendar(string summary, string timeZone)
-	{
-		var newCalendar = new Calendar
-		{
-			Summary = summary,
-			TimeZone = timeZone
-		};
+    public async Task<Calendar> CreateCalendar(string summary, string timeZone)
+    {
+        var newCalendar = new Calendar
+        {
+            Summary = summary,
+            TimeZone = timeZone
+        };
 
-		var createdCalendar = _service.Calendars.Insert(newCalendar).Execute();
-
+        var createdCalendar = await _service.Calendars.Insert(newCalendar).ExecuteAsync();
         return createdCalendar;
     }
 }
