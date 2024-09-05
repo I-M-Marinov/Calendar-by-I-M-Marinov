447f8537c21f803d56baaac2d527af715f550aed
I-M-Marinov <lcfrrr@gmail.com>
2024-09-05 21:00:15 +0300

    EventViewModel updated
    EventViewModel updated
    
    -- Added two lists to help with the option when creating/updating an event ( List<SelectListItem>

--- Calendar-by-I-M-Marinov/Models/EventViewModel.cs
+++ Calendar-by-I-M-Marinov/Models/EventViewModel.cs
@@ -1,28 +1,33 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Calendar_by_I_M_Marinov.Models
 {
     public class EventViewModel
     {
         public string EventId { get; set; }
+        public string CalendarId { get; set; } = null!;
         public string Summary { get; set; } = null!;
 
         public string? Description { get; set; }
 
         public string? Location { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-ddTHH:mm}", ApplyFormatInEditMode = true)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-ddTHH:mm}", ApplyFormatInEditMode = true)]
         public DateTime? Start { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-ddTHH:mm}", ApplyFormatInEditMode = true)]
         public DateTime? End { get; set; }
 
-        public string Visibility { get; set; } = "public"; // Default to public
-        public string EventType { get; set; } = "single"; // Options: single, annual, allDay // Default to single
+        public string Visibility { get; set; }  
+        public string EventType { get; set; }  // Options: single, annual, allDay 
         public List<string> Attendants { get; set; } = new();
         public string SendUpdates { get; set; } // Options: all, externalOnly, none 
 
+        public List<SelectListItem> VisibilityOptions { get; set; }
+        public List<SelectListItem> EventTypeOptions { get; set; }
 
-    }
+
+	}
 
 }
