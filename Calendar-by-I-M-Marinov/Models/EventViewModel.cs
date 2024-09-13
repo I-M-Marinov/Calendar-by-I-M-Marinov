a2f1347f5b58660db68cb3ccdab44c049f1b8180
I-M-Marinov <lcfrrr@gmail.com>
2024-09-13 22:30:10 +0300

    Add the RecurringEventId  property to the EventViewModel
    Add the RecurringEventId  property to the EventViewModel

--- Calendar-by-I-M-Marinov/Models/EventViewModel.cs
+++ Calendar-by-I-M-Marinov/Models/EventViewModel.cs
@@ -5,9 +5,10 @@ namespace Calendar_by_I_M_Marinov.Models
 {
     public class EventViewModel
     {
-        public string EventId { get; set; }
-        public string CalendarId { get; set; } 
-        public string Summary { get; set; } = null!;
+        public string EventId { get; set; } = null!;
+		public string CalendarId { get; set; } = null!;
+		public string? RecurringEventId { get; set; }
+		public string Summary { get; set; } = null!;
 
         public string? Description { get; set; }
 
@@ -24,8 +25,9 @@ namespace Calendar_by_I_M_Marinov.Models
         public bool IsAllDayEvent { get; set; }
         public List<string> Attendants { get; set; } = new();
         public string SendUpdates { get; set; } // Options: all, externalOnly, none 
+       
 
-        public List<SelectListItem> VisibilityOptions { get; set; }
+		public List<SelectListItem> VisibilityOptions { get; set; }
         public List<SelectListItem> EventTypeOptions { get; set; }
 
 
