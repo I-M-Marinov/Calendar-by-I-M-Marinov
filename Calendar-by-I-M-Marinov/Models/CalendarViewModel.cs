dfdbe265d9856777b7c10038296941da5f2d0d67
I-M-Marinov <lcfrrr@gmail.com>
2024-09-15 11:47:30 +0300

    Add the RecurringEventId  property to all the views that would need it
    Add the RecurringEventId  property to all the views that would need it

--- Calendar-by-I-M-Marinov/Models/CalendarViewModel.cs
+++ Calendar-by-I-M-Marinov/Models/CalendarViewModel.cs
@@ -5,7 +5,7 @@ namespace Calendar_by_I_M_Marinov.Models
 	public class CalendarViewModel
 	{
 		public string CalendarId { get; set; } = null!;
-		public string CalendarName { get; set; } = null!;
+        public string CalendarName { get; set; } = null!;
 		public string Description { get; set; } = null!;
 		public string AccessRole { get; set; } = null!;
 		public int EventsCount { get; set; }
--- Calendar-by-I-M-Marinov/Models/DeleteEventViewModel.cs
+++ Calendar-by-I-M-Marinov/Models/DeleteEventViewModel.cs
@@ -3,12 +3,15 @@
 	public class DeleteEventViewModel
 	{
 		public string EventId { get; set; }
+		public string CalendarId { get; set; } = null!;
+		public string? RecurringEventId { get; set; }
 		public bool DeleteSeries { get; set; } = false; // default to false
 		public string Summary { get; set; }
 		public DateTime? Start { get; set; }
 		public DateTime? End { get; set; }
 		public string Location { get; set; }
-        public string CalendarId { get; set; } = null!;
+        public bool IsAllDayEvent { get; set; } 
+
 
     }
 
