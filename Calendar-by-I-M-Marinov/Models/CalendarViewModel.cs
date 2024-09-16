60e82e0beaa1ea0d104afa8d4079f13a10be21d8
I-M-Marinov <lcfrrr@gmail.com>
2024-09-16 19:18:28 +0300

    Update the CalendarViewModel
    Update the CalendarViewModel

--- Calendar-by-I-M-Marinov/Models/CalendarViewModel.cs
+++ Calendar-by-I-M-Marinov/Models/CalendarViewModel.cs
@@ -1,17 +1,18 @@
-﻿using Google.Apis.Calendar.v3.Data;
+﻿using System.Runtime.CompilerServices;
+using Google.Apis.Calendar.v3.Data;
 
 namespace Calendar_by_I_M_Marinov.Models
 {
 	public class CalendarViewModel
 	{
-		public string CalendarId { get; set; } = null!;
-        public string CalendarName { get; set; } = null!;
-		public string Description { get; set; } = null!;
-		public string AccessRole { get; set; } = null!;
-		public int EventsCount { get; set; }
+		public string CalendarId { get; set; } 
+        public string CalendarName { get; set; } 
+		public string? Description { get; set; } 
+		public string? AccessRole { get; set; } 
+		public int? EventsCount { get; set; }
 		public bool? Primary { get; set; }
 
-		// List of events associated with this calendar
-		public List<Event> Events { get; set; } = new List<Event>();
+        // List of events associated with this calendar
+        public List<Event> Events { get; set; } = new List<Event>();
     }
 }
