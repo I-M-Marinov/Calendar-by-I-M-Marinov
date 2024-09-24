ca752561ad2cec8804531f6b62e666e038566086
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-09-24 18:22:09 +0300

    Created the ContactViewModel
    Created the ContactViewModel

---
+++ Calendar-by-I-M-Marinov/Models/People/ContactViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Calendar_by_I_M_Marinov.Models.People
+{
+	public class ContactViewModel
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Birthday { get; set; } // Birthday as a string
+	}
+
+}
