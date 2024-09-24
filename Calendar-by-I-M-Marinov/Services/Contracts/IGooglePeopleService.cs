d010b523786a010c1eaa5db02a95ee6f0cc1216e
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-09-24 18:22:55 +0300

    Added the GetAllContactsAsync to the interface
    Added the GetAllContactsAsync to the interface

--- Calendar-by-I-M-Marinov/Services/Contracts/IGooglePeopleService.cs
+++ Calendar-by-I-M-Marinov/Services/Contracts/IGooglePeopleService.cs
@@ -1,7 +1,13 @@
-﻿namespace Calendar_by_I_M_Marinov.Services.Contracts
+﻿using Calendar_by_I_M_Marinov.Models.People;
+using Google.Apis.PeopleService.v1.Data;
+
+namespace Calendar_by_I_M_Marinov.Services.Contracts
 {
     public interface IGooglePeopleService
     {
         Task<string> FetchDisplayNameByEmailAsync(string email);
+        Task<List<ContactViewModel>> GetAllContactsAsync();
+
+
     }
 }
