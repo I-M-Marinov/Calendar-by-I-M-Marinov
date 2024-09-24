bb3b893a627b4d904d4796c5e4e32093d9346c84
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-09-24 18:21:45 +0300

    Created the PeopleController class
    Created the PeopleController class
    
    - Added the GetAllContacts() method that would use the _peopleGoogleService to get all contacts and pass them to the View;

---
+++ Calendar-by-I-M-Marinov/Controllers/PeopleController.cs
@@ -0,0 +1,45 @@
+﻿using Calendar_by_I_M_Marinov.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Calendar_by_I_M_Marinov.Models.People;
+
+
+namespace Calendar_by_I_M_Marinov.Controllers
+{
+	public class PeopleController : Controller
+	{
+		private readonly IGooglePeopleService _peopleGoogleService;
+
+
+		public PeopleController(IGooglePeopleService peopleGoogleService)
+		{
+			_peopleGoogleService = peopleGoogleService;
+		}
+
+
+		[HttpGet]
+		[Route("contacts")]
+		public async Task<IActionResult> GetAllContacts()
+		{
+			try
+			{
+				var contacts = await _peopleGoogleService.GetAllContactsAsync();
+
+				if (contacts.Count == 0)
+				{
+					return NotFound("No contacts found.");
+				}
+				
+				ViewBag.ContactsCount = contacts.Count;
+
+				return View(contacts);
+			}
+			catch (System.Exception ex)
+			{
+				return StatusCode(500, $"Internal server error: {ex.Message}");
+			}
+
+
+		}
+
+	}
+}
