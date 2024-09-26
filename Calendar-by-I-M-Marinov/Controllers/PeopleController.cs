59eeab8ca34a99182570c496d33ca79ab245cdfa
I-M-Marinov <lcfrrr@gmail.com>
2024-09-26 18:30:07 +0300

    Update the PeopleController
    Update the PeopleController
    
    - Added a GET and POST methods ( Index ) to be used in the GetAllContacts View to show the label ( group ) of every contact

--- Calendar-by-I-M-Marinov/Controllers/PeopleController.cs
+++ Calendar-by-I-M-Marinov/Controllers/PeopleController.cs
@@ -1,6 +1,7 @@
 ﻿using Calendar_by_I_M_Marinov.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
 using Calendar_by_I_M_Marinov.Models.People;
+using Google.Apis.PeopleService.v1.Data;
 
 
 namespace Calendar_by_I_M_Marinov.Controllers
@@ -46,6 +47,80 @@ namespace Calendar_by_I_M_Marinov.Controllers
 
 
 		}
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                var contacts = await _peopleGoogleService.GetAllContactsAsync();
 
-	}
+                var groupedContacts = contacts
+                    .Where(c => c.Labels != null && c.Labels.Count > 0) 
+                    .GroupBy(c => c.Labels.First()) 
+                    .OrderBy(g => g.Key) 
+                    .ToList();
+
+                ViewBag.ContactsCount = contacts.Count; 
+                ViewBag.ContactGroups = await _peopleGoogleService.GetContactGroupsAsync(); 
+
+                return View(groupedContacts); 
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}"); 
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(string selectedGroup)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(selectedGroup))
+                {
+                    ModelState.AddModelError("", "Please select a contact group.");
+                    ViewBag.ContactGroups = await _peopleGoogleService.GetContactGroupsAsync();
+                    return View(new List<IGrouping<char, Person>>());
+                }
+
+                var contactGroup = await _peopleGoogleService.GetContactGroupAsync(selectedGroup);
+
+                var contacts = new List<Person>();
+
+                if (contactGroup.MemberResourceNames != null && contactGroup.MemberResourceNames.Count > 0)
+                {
+                    foreach (var memberResourceName in contactGroup.MemberResourceNames)
+                    {
+                        var person = await _peopleGoogleService.GetPersonAsync(memberResourceName);
+                        if (person != null)
+                        {
+                            contacts.Add(person);
+                        }
+                    }
+                }
+
+                var groupedContacts = contacts
+                    .Where(c => c.Names != null && c.Names.Count > 0) 
+                    .GroupBy(c => char.ToUpper(c.Names[0].DisplayName[0])) 
+                    .OrderBy(g => g.Key) 
+                    .ToList();
+
+                ViewBag.ContactsCount = contacts.Count; 
+                ViewBag.ContactGroups = await _peopleGoogleService.GetContactGroupsAsync(); 
+
+                return View(groupedContacts);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+
+
+
+
+    }
+
 }
+
