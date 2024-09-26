a0debf72c8b4d180508b08338747e04ad5a3527d
I-M-Marinov <lcfrrr@gmail.com>
2024-09-26 18:31:48 +0300

    Updated Program.cs
    Updated Program.cs
    
    --> added endpoints for the People Controller / Index action

--- Calendar-by-I-M-Marinov/Program.cs
+++ Calendar-by-I-M-Marinov/Program.cs
@@ -66,4 +66,11 @@ app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=People}/{action=Index}/{id?}");
+});
+
 app.Run();
