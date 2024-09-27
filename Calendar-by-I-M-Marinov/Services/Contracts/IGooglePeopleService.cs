f1cc3940983526327318c21e4c5f481c6559f6aa
I-M-Marinov <lcfrrr@gmail.com>
2024-09-27 21:42:23 +0300

    Update the IGooglePeopleService interface
    Update the IGooglePeopleService interface
    
    -- Added an overload for the GetAllContactsAsync method that would take a parameter string selectedGroup and returns a list of the ContactViewModel

--- Calendar-by-I-M-Marinov/Services/Contracts/IGooglePeopleService.cs
+++ Calendar-by-I-M-Marinov/Services/Contracts/IGooglePeopleService.cs
@@ -7,6 +7,7 @@ namespace Calendar_by_I_M_Marinov.Services.Contracts
     {
         Task<string> FetchDisplayNameByEmailAsync(string email);
         Task<List<ContactViewModel>> GetAllContactsAsync();
+        Task<List<ContactViewModel>> GetAllContactsAsync(string selectedGroup);
         Task<List<ContactGroup>> GetContactGroupsAsync();
         Task<ContactGroup> GetContactGroupAsync(string groupResourceName);
         Task<Person> GetPersonAsync(string personResourceName);
