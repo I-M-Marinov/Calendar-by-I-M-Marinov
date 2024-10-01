0986a2e66e321c144751319900a4d20c04dd42b5
I-M-Marinov <lcfrrr@gmail.com>
2024-10-01 11:05:52 +0300

    Updated the DateTimeExtensions in the Common folder
    Updated the DateTimeExtensions in the Common folder
    
    -- Added ParseBirthday which would get the string birthday and parse it to DateTime

--- Calendar-by-I-M-Marinov/Common/DateTimeExtensions.cs
+++ Calendar-by-I-M-Marinov/Common/DateTimeExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Calendar_by_I_M_Marinov.Common
+﻿using Google.Apis.PeopleService.v1.Data;
+
+namespace Calendar_by_I_M_Marinov.Common
 {
 	public static class DateTimeExtensions
 	{
@@ -21,8 +23,29 @@
 
         public static DateTime ToLocalTime(this DateTime dateTime)
         {
-            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Europe/Sofia"); // Adjust as necessary
+            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Europe/Sofia"); 
             return TimeZoneInfo.ConvertTime(dateTime, timeZoneInfo);
         }
-    }
+
+        public static List<Birthday> ParseBirthday(string birthday)
+        {
+	        if (DateTime.TryParse(birthday, out DateTime parsedDate))
+	        {
+		        return new List<Birthday>
+		        {
+			        new Birthday
+			        {
+				        Date = new Date
+				        {
+					        Day = parsedDate.Day,
+					        Month = parsedDate.Month,
+					        Year = parsedDate.Year
+				        }
+			        }
+		        };
+	        }
+
+	        return null; 
+        }
+	}
 }
